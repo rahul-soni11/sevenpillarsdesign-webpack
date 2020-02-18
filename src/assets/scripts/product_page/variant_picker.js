e6669ba4600ad34dae6b8da0dd2c85070f547f00
Rahul Soni <rahulsoni@Rahuls-MacBook-Air.local>
2020-02-18 17:00:54 +0530

    Improved Variant Picker

--- src/assets/scripts/product_page/variant_picker.js
+++ src/assets/scripts/product_page/variant_picker.js
@@ -3,13 +3,17 @@ var $ = require("jquery");
 // json Data provided by backend, 
 var data = {}
 
-// key value pair of attribute:id
-var current_pair = {}
+// holds unique key of currentely selected variant
+var current_variant_key = {}
+
+var current_variant_attributes = {}
+
+var element = null
 
 window.onload = $(document).ready(function () {
 
     // select #variant-picker element
-    const element = document.getElementById('variant-picker');
+    element = document.getElementById('variant-picker');
 
     // if element exist 
     if (element != null) {
@@ -17,10 +21,9 @@ window.onload = $(document).ready(function () {
         data = JSON.parse(element.getAttribute('data-variant-picker-data'))
 
         // initially set first variant as activated
-        const key = Object.keys(data.variants)[0]
-
-        current_pair = JSON.parse(JSON.stringify(data.variants[key].attributes))
-        set_variant(key)
+        current_variant_key = Object.keys(data.variants)[0]
+        current_variant_attributes = data.variants[current_variant_key].attributes
+        set_variant(current_variant_key)
 
         // generate variant picker html notes for each attribute
         $.each(data["variantAttributes"], function () {
@@ -48,13 +51,14 @@ function get_variantFragment(attribute_id, name, values) {
     for (value of values) {
         var label = document.createElement('label')
 
-        //first variant attribute active
-        if (current_pair[attribute_id] == value.pk) {
+        // first variant attribute active
+        if (data.variants[current_variant_key].attributes[attribute_id] == value.pk) {
             label.setAttribute('class', 'btn btn-secondary active')
         }
         else {
             label.setAttribute('class', 'btn btn-secondary')
         }
+
         label.textContent = value.name
         label.setAttribute('name', attribute_id)
         label.setAttribute('id', value.pk)
@@ -79,32 +83,48 @@ function get_variantFragment(attribute_id, name, values) {
 // The .on() method is used to delegate events to elements, dynamically added or already present in the DOM:
 // STATIC-PARENT     on  EVENT    DYNAMIC-CHILD
 $('#variant-picker').on('click', '.btn', function () {
-    // if any button clicked inside #variant-picker element update data in current_pair variable
-    attribute = this.getAttribute('name');
+    // if any button clicked inside #variant-picker element update the current_pair variable
+    attribute_id = this.getAttribute('name');
     value = this.getAttribute('id');
-    current_pair[attribute] = value
-    console.log(current_pair)
-    pick_variant()
+    current_variant_attributes[attribute_id] = value
+
+    uk = []
+    for (const [key, value] of Object.entries(current_variant_attributes)) {
+        uk.push(key)
+        uk.push(value)
+    }
+    // console.log(uk.join(''))
+    pick_variant(uk.join(''))
 });
 
-function pick_variant() {
+function pick_variant(variant_Uk) {
     // selects variaint by comparing current_pair and attributes object in all variants in data[variants]
     // loop through each variant in data
     found = false
-
-    $.each(data["variants"], function (index) {
-        if (JSON.stringify(this["attributes"]) === JSON.stringify(current_pair)) {
-            set_variant(this["id"])
+    cartButton = document.getElementById('product-addtocart-button')
+    // check variant exist
+    if (data.variants.hasOwnProperty(variant_Uk)) {
+        set_variant(variant_Uk)
+        // check variant availability
+        if (data.variants[variant_Uk].availability) {
             found = true
-            return false;
+        } else {
+            // variant out of stock
+            cartButton.innerText = 'Variant Out of Stock'
         }
-    });
+    } else {
+        // variant not exist
+        // set price to null
+        cartButton.innerText = 'Variant not available'
+    }
 
     // disable add to cart button
     if (found == true) {
-        $("#product-addtocart-button").removeAttr("disabled")
+        cartButton.removeAttribute("disabled")
+        cartButton.textContent = 'Add to Cart'
+
     } else {
-        $("#product-addtocart-button").attr("disabled", "")
+        cartButton.setAttribute("disabled", "")
     }
 }
 
@@ -114,5 +134,5 @@ function set_variant(variant_id) {
     document.getElementById('variant-price-component').innerText = data["variants"][variant_id]["price"]
 
     // set current_variant id to hidden input for submiting form
-    document.getElementById('id_variant').value = variant_id
+    document.getElementById('id_variant').value = data.variants[variant_id].id
 }
